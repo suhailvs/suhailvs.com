600cae73b726f25694e1f106c2ed152dbd3df1d7
suhailvs <suhailvs@gmail.com>
2014-06-18 06:35:40 +0530

    setup openshift 53a0d9e05973cad542000331@www-suhailvs.rhcloud.com

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+
+# Activate the virtual env
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:pass
+
+# set the variable DJANGO_SETTINGS_MODULE="mysite.settings" to environ
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+# add OPENSHIFT_REPO_DIR='~/app-root/repo/www/' to system path
+sys.path.append(os.environ['OPENSHIFT_REPO_DIR'])
+
+# taken from django1.6 wsgi file
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
