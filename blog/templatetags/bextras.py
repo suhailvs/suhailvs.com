029460e3d5e13fbe3eb710f7b2dafd3b0a79a1d1
suhailvs <suhailvs@gmail.com>
2014-06-20 07:43:54 +0530

    updated tagging

--- blog/templatetags/bextras.py
+++ blog/templatetags/bextras.py
@@ -1,8 +1,21 @@
 from django import template
 from django.utils.safestring import mark_safe
 import markdown as mkdn
+from blog.models import Blog,Tag
+from django.core.urlresolvers import reverse
 register = template.Library()
 
 @register.filter
 def markdown(value,smode=None):
     return mark_safe(mkdn.markdown(value))
+
+@register.simple_tag
+def create_tagmenu():
+    tgs=Tag.objects.all()
+    if not tgs:return ''
+    taglist=[(Blog.objects.filter(tags=tg).distinct().count(),tg.pk,tg.name) for tg in tgs]    
+    str_html=''
+    turl=reverse('home')
+    for tg in taglist:
+        str_html+='<li><a href="{url}?tags={tag[1]}"><span class="label label-warning">{tag[2]}</span> x {tag[0]}</a></li>'.format(tag=tg,url=turl)
+    return str_html
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,13 @@ from blog.models import Blog,Tag
 
 # Create your views here.
 def home(request):
-	blogs=Blog.objects.all()
+	if 'tags' in request.GET:
+		# get note list base on tags in post, only public notes
+		#tags=request.GET.getlist('tags')
+		blogs=Blog.objects.filter(tags__in=request.GET.getlist('tags')).order_by('-created')
+		tags=Tag.objects.filter(id__in=request.GET.getlist('tags'))	
+	else:
+		blogs=Blog.objects.order_by('-created')
 	return render(request,'home.html',{'blogs':blogs})
 
 def blog(request,pk):
