50ba7420795cabccd24a4d092fa1c01f739e2890
suhailvs <suhailvs@gmail.com>
2014-06-21 07:58:36 +0530

    updated pagination and archive links

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from blog.models import Blog,Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def limit_data(page,data,n=10):
+	paginator = Paginator(data,n) # Show 10 notes per page
+	try:
+		p_data = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		p_data = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		p_data = paginator.page(paginator.num_pages)
+	return p_data
+
 
 # Create your views here.
 def home(request):
@@ -9,8 +23,14 @@ def home(request):
 	else:
 		filtered_tags=False
 		blogs=Blog.objects.order_by('-created')
+	blogs=limit_data(request.GET.get('page'),blogs,1)
 	return render(request,'home.html',{'blogs':blogs,'filtered_tags':filtered_tags})
 
+def archive(request,year,month):
+	filtered_by='Year:{0}, Month:{1}'.format(year,month)
+	blogs=Blog.objects.filter(created__year=year,created__month=month).order_by('-created')
+	blogs=limit_data(request.GET.get('page'),blogs,1)
+	return render(request,'archive.html',{'blogs':blogs,'filtered_by':filtered_by})
 def blog(request,pk):
 	blog=Blog.objects.get(id=pk)
 	return render(request,'blog.html',{'blog':blog})
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('blog.views',
     # Examples:
-     url(r'^$', 'blog.views.home', name='home'),
-     url(r'^(\d{1,5})/', 'blog.views.blog', name='blog'),
+    url(r'^$', 'home', name='home'),
+    url(r'^(\d{1,5})/', 'blog', name='blog'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^(aboutme|projects)$', 'blog.views.staticpages', name='static_pages'),
+    url(r'^archive/(\d{4})/(\d{1,2})/$', 'archive', name='archive'),
+    url(r'^(aboutme|projects)$', 'staticpages', name='static_pages'),
     url(r'^admin/', include(admin.site.urls)),
 )
