4b88c6fb5ba28b22af07c11ac1c9882df4c38ee8
suhailvs <suhailvs@gmail.com>
2014-06-22 23:38:51 +0530

    updated sitemaps of google

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Tag(models.Model):
@@ -15,3 +16,5 @@ class Blog(models.Model):
 	updated = models.DateTimeField(auto_now=True)
 	def __unicode__(self):
 		return unicode(self.title)
+	def get_absolute_url(self):
+		return '{url}{slug}/'.format(url=reverse('blog', args=(self.id,)), slug=self.title.replace(' ','-'))
--- mysite/urls.py
+++ mysite/urls.py
@@ -11,3 +11,25 @@ urlpatterns = patterns('blog.views',
     url(r'^(aboutme|projects)$', 'staticpages', name='static_pages'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+from django.contrib.sitemaps import GenericSitemap
+from blog.models import Blog,Tag
+
+info_dict = {
+    'queryset': Blog.objects.all(),
+    'date_field': 'created',
+}
+
+sitemaps = {    
+    'blog': GenericSitemap(info_dict, priority=0.6),
+}
+
+urlpatterns += patterns('',
+    # some generic view using info_dict
+    # ...
+
+    # the sitemap
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+)
